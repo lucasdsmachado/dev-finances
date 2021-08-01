9eaa9ee22d1ad7ee0cbbcb56ac38814fc617cbc7
lucasdsmachado <brian.machado@ufba.br>
2021-08-01 12:35:30 -0300

    added transactions objects

--- scripts.js
+++ scripts.js
@@ -13,3 +13,56 @@ const modal = {
   }
 }
 
+const transactions = [
+{
+  id: 1,
+  description: 'Luz',
+  amount: -50000,
+  date: '23/01/2021'
+},
+{
+  id: 2,
+  description: 'Website',
+  amount: 500000,
+  date: '23/01/2021'
+},
+{
+  id: 1,
+  description: 'Internet',
+  amount: -20000,
+  date: '23/01/2021'
+}
+]
+
+const transaction = {
+  incomes() {
+
+  },
+  expenses() {
+
+  },
+  total() {
+
+  }
+}
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  
+  addTransaction (transactions, index) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = DOM.innerHTMLTransaction(transactions);
+  },
+  innerHTMLTransaction(transactions) {
+
+    const html = `
+    <td class="description">${transactions.description}</td>
+    <td class="expense">${transactions.amount}</td>
+    <td>${transactions.date}</td>
+    <td>
+      <img src="./assets/minus.svg" alt="icone menos">
+    </td>
+    `;
+    return html;
+  }
+}
