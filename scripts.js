287dc1dc656adf83943f3c62081cfda7f123dd67
lucasdsmachado <brian.machado@ufba.br>
2021-08-01 20:32:34 -0300

    added validate fields function

--- scripts.js
+++ scripts.js
@@ -125,7 +125,31 @@ const Utils = {
 }
 
 const Form = {
-  
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+      return {
+          description: Form.description.value,
+          amount: Form.amount.value,
+          date: Form.date.value
+      }
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+    if(description.trim === "" ||
+       amount.trim === "" ||
+       date.trim === "") {
+         throw new Error("Por favor, preencha todos os campos");
+       }
+  },
+
+  submit(event) {
+    event.preventDefault()
+    Form.validateFields()
+  }
 }
 
 const App = {
