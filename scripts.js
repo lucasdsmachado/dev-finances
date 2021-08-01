2c8c1474acb44eee9c5509a65ad31d206027e70e
lucasdsmachado <brian.machado@ufba.br>
2021-08-01 12:32:35 -0300

    added modal open/close functions

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const modal = {
+  open(){
+    document
+      .querySelector('.modal-overlay')
+      .classList
+      .add('active')
+  },
+  close(){
+      document 
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+  }
+}
+
